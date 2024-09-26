94d83de0720c820d271fc783d73ceb5ca9a0960e
NOTNA9015 <162547923+NOTNA9015@users.noreply.github.com>
2024-09-26 10:18:31 +0200

    Add files via upload

---
+++ Cobat/cobat.source.bat
@@ -0,0 +1,131 @@
+@echo off
+Title Cobat & Color 1
+chcp 65001 >nul 2>&1
+:BannerSlow
+echo.
+echo.
+echo      ██████╗
+echo     ██╔════╝
+echo     ██║
+echo     ██║
+echo     ╚██████╗ 
+echo      ╚═════╝
+echo.
+echo.
+TIMEOUT /T 1 >nul 2>&1
+cls
+
+echo.
+echo.
+echo      ██████╗ ██████╗
+echo     ██╔════╝██╔═══██╗
+echo     ██║     ██║   ██║
+echo     ██║     ██║   ██║ 
+echo     ╚██████╗╚██████╔╝ 
+echo      ╚═════╝ ╚═════╝
+echo.
+echo.
+TIMEOUT /T 1 >nul 2>&1
+cls
+
+echo.
+echo.
+echo      ██████╗ ██████╗ ██████╗
+echo     ██╔════╝██╔═══██╗██╔══██╗
+echo     ██║     ██║   ██║██████╔╝
+echo     ██║     ██║   ██║██╔══██╗
+echo     ╚██████╗╚██████╔╝██████╔╝
+echo      ╚═════╝ ╚═════╝ ╚═════╝
+echo.
+echo.
+TIMEOUT /T 1 >nul 2>&1
+cls
+
+echo.
+echo.
+echo      ██████╗ ██████╗ ██████╗  █████╗
+echo     ██╔════╝██╔═══██╗██╔══██╗██╔══██╗
+echo     ██║     ██║   ██║██████╔╝███████║
+echo     ██║     ██║   ██║██╔══██╗██╔══██║
+echo     ╚██████╗╚██████╔╝██████╔╝██║  ██║
+echo      ╚═════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝
+echo.
+echo.
+TIMEOUT /T 1 >nul 2>&1
+cls
+
+echo.
+echo.
+echo      ██████╗ ██████╗ ██████╗  █████╗ ████████╗
+echo     ██╔════╝██╔═══██╗██╔══██╗██╔══██╗╚══██╔══╝
+echo     ██║     ██║   ██║██████╔╝███████║   ██║   
+echo     ██║     ██║   ██║██╔══██╗██╔══██║   ██║   
+echo     ╚██████╗╚██████╔╝██████╔╝██║  ██║   ██║   
+echo      ╚═════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   
+echo.
+echo.
+REM Slow download. For fun. 
+cls
+call :Banner
+TIMEOUT /T 1 >nul 2>&1
+echo Cobat 3.0 and above is not open source
+TIMEOUT /T 2 >nul 2>&1
+echo Downloading latest open source version
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloading.
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloading..
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloading...
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloading.
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloading..
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloading...
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloading.
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloading..
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloading...
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloading.
+TIMEOUT /T 1 >nul 2>&1
+cls
+call :Banner
+echo Downloaded
+start https://github.com/NOTNA9015/Cobat/releases/download/Pull/cobat.bat
+pause & exit
+
+:Banner
+echo.
+echo.
+echo      ██████╗ ██████╗ ██████╗  █████╗ ████████╗
+echo     ██╔════╝██╔═══██╗██╔══██╗██╔══██╗╚══██╔══╝
+echo     ██║     ██║   ██║██████╔╝███████║   ██║   
+echo     ██║     ██║   ██║██╔══██╗██╔══██║   ██║   
+echo     ╚██████╗╚██████╔╝██████╔╝██║  ██║   ██║   
+echo      ╚═════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝   ╚═╝   
+echo.
+echo.
